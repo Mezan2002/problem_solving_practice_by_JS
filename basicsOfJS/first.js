8f5e5a0692d3b446fc0d5e5d491a3e528f02c550
Mezan2002 <mezanurr009@gmail.com>
2023-11-07 17:56:56 +0600

    started with the simple knowdage of variables

---
+++ basicsOfJS/first.js
@@ -0,0 +1,4 @@
+// console.log is written for print or show an output in the console
+
+console.log(1); // output => 1
+console.log("Hello world!"); //output => Hello world!
---
+++ basicsOfJS/variable.js
@@ -0,0 +1,27 @@
+// all programming languages has that variable
+// variables are used for save a value for another use and it could be changed
+// vary + able = variable which means that thing which can be changed at any time like your age
+// there are 3 keywords to declare a variable in JS but we will know only the var in that time
+// different programming languages has different style to write the variables in that case we are learing JavaScript so in javascript the variable written style is down below:
+// 1. declare the variable keyword which is var
+// 2. give a name of the variable
+// 3. give an qual sign
+// 4. then assign the value of the variable,
+// 5. use the semi colon (;) sign to end the variable
+// example down below:
+
+var age = 21;
+
+// :::best practices about creating a variable:::
+// 1. a variable name should be understandable and unique
+// 2. variable names aren't starts with any type of number if you need a number then add the number at the end of the name
+// 3. variable name wouldn't take any space or blank places
+// 4. if the variable name contains more than one word then you can use the underscore (is_single) or the camel casing (isSingle)
+
+// task : create 5 variables for practicing the variable declaration
+
+var tempareture = 40;
+var price = 500;
+var rollNumber = 5;
+var number_of_cars = 0;
+var number_of_mobiles = 4;
